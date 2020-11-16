4c1727f4657da7c6e7edbd30ac3e592dea717c6c
Marcelo <marcelo.miglioli@hotmail.com>
2020-11-16 17:25:27 -0300

    Configurando Layout Padrão e Controller/Viwer Cursos

---
+++ SCIR/SCIR/Controllers/CursosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCIR.Controllers
+{
+    public class CursosController : Controller
+    {
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
